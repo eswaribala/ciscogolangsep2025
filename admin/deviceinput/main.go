35ad148b7cb31fcd37a1babe6fa1c923f5dc90e7
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-22 12:52:55 +0530

    updated

--- admin/deviceinput/main.go
+++ admin/deviceinput/main.go
@@ -9,15 +9,15 @@ func main() {
 	var deviceName string
 	var IPAddress string
 	var deviceType string
+	fmt.Println("Enter device details (ID, Name, IP Address, Type) separated by spaces:")
+	_, err := fmt.Scan(&deviceId, &deviceName, &IPAddress, &deviceType)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
 
-  _,err:= fmt.Scan(&deviceId, &deviceName, &IPAddress, &deviceType)
-  if err != nil {
-	fmt.Println("Error reading input:", err)
-	return
-  }
-
-  fmt.Println("Device ID:", deviceId)
-  fmt.Println("Device Name:", deviceName)
-  fmt.Println("IP Address:", IPAddress)
-  fmt.Println("Device Type:", deviceType)
+	fmt.Println("Device ID:", deviceId)
+	fmt.Println("Device Name:", deviceName)
+	fmt.Println("IP Address:", IPAddress)
+	fmt.Println("Device Type:", deviceType)
 }
