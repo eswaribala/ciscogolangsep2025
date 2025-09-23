f42b5db80fa4a73339cd18caf5577d18d4115fbd
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-23 10:57:37 +0530

    updated

--- admin/networkstructapp/main.go
+++ admin/networkstructapp/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
-	
+
 	domain := models.NewDomain()
-	fmt.Printf("Domain: %+v\n", domain)
+	fmt.Printf("Domain: %s\n", domain.ID)
+	fmt.Printf("Name: %s\n", domain.Name)
+	fmt.Printf("Description: %s\n", domain.Description)
+	//fmt.Printf("Subnets: %+v\n", domain.Subnets)
+	for _, subnet := range domain.Subnets {
+		fmt.Printf("Subnet ID: %s, CIDR: %s, Gateway: %+v\n", subnet.ID, subnet.CIDR, subnet.GatewayInstance)
+	}
 
 }
