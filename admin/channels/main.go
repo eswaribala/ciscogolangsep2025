e7112b760777242f4a5cc74b1019c67928aa7bf1
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-24 11:16:02 +0530

    updated

--- admin/channels/main.go
+++ admin/channels/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func checkLink(link string) {
+func checkLink(link string, channel chan string) {
 
 	println("Checking link:", link)
 	resp, err := http.Get(link)
@@ -16,18 +16,28 @@ func checkLink(link string) {
 	}
 
 	println(link + " - " + resp.Status)
+	//send the message to the channel
+	channel <- link + " - " + resp.Status
 
 }
 
 func main() {
 
+	//create the channel
+	channel := make(chan string)
+
 	link := []string{"https://www.google.com", "https://www.facebook.com", "https://www.youtube.com", "https://www.twitter.com", "https://www.instagram.com"}
 
 	for _, url := range link {
 		//rotines
-		go checkLink(url)
+		go checkLink(url, channel)
+		//read the message from the channel
+		println(<-channel)
 
 	}
+	//waiting to read all messages from the channel
+
+	//println(<-channel)
 
 	println("All links have been checked.")
 
