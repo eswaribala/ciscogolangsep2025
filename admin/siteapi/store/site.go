8850662d1c73be8922d01c004be9002500b3e09a
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-25 15:12:23 +0530

    updated

--- admin/siteapi/store/site.go
+++ admin/siteapi/store/site.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Site struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"type:varchar(100);uniqueIndex;not null"`
-	Location string `gorm:"type:varchar(100);not null"`
-	Status   bool   `gorm:"not null;default:true"`
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	Name     string `json:"name" gorm:"type:varchar(100);uniqueIndex;not null"`
+	Location string `json:"location" gorm:"type:varchar(100);not null"`
+	Status   bool   `json:"status" gorm:"not null;default:true"`
 }
 
 func SaveSiteInfo(writer *http.ResponseWriter, request *http.Request) {
@@ -36,17 +36,58 @@ func SaveSiteInfo(writer *http.ResponseWriter, request *http.Request) {
 }
 
 func GetAllSiteInfo(writer *http.ResponseWriter, request *http.Request) {
+	var sites []Site
+	db := MySQLConnectionHelper()
+	result := db.Find(&sites)
+	if result.Error != nil {
+
+		http.Error(*writer, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	//return the site info to client
+	json.NewEncoder(*writer).Encode(sites)
 
 }
 
 func GetSiteInfoByID(writer *http.ResponseWriter, request *http.Request) {
+	var site Site
+	db := MySQLConnectionHelper()
+	result := db.First(&site, request.URL.Query().Get("id"))
+	if result.Error != nil {
+
+		http.Error(*writer, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	//return the site info to client
+	json.NewEncoder(*writer).Encode(site)
 
 }
 
 func UpdateSiteInfoByID(writer *http.ResponseWriter, request *http.Request) {
+	var site Site
+	db := MySQLConnectionHelper()
+	err := json.NewDecoder(request.Body).Decode(&site)
+	if err != nil {
+		http.Error(*writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	result := db.Model(&site).Updates(Site{Name: site.Name, Location: site.Location, Status: site.Status})
+	//send result
+	if result.Error != nil {
+		http.Error(*writer, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(*writer).Encode(site)
 
 }
 
 func DeleteSiteInfoByID(writer *http.ResponseWriter, request *http.Request) {
+	db := MySQLConnectionHelper()
+	result := db.Delete(&Site{}, request.URL.Query().Get("id"))
+	if result.Error != nil {
+		http.Error(*writer, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(*writer).Encode("Site deleted successfully")
 
 }
