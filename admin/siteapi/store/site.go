ec1b1fc944c2d35113ff591aaf0b26c7d931728a
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-25 15:01:55 +0530

    updated

--- admin/siteapi/store/site.go
+++ admin/siteapi/store/site.go
@@ -1,9 +1,52 @@
 package store
 
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Site struct {
-	ID               uint   `gorm:"primaryKey"`
-	Name             string `gorm:"type:varchar(100);uniqueIndex;not null"`
-	Location         string `gorm:"type:varchar(100);not null"`	
-	Status           bool `gorm:"not null;default:true"`
+	ID       uint   `gorm:"primaryKey"`
+	Name     string `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Location string `gorm:"type:varchar(100);not null"`
+	Status   bool   `gorm:"not null;default:true"`
+}
+
+func SaveSiteInfo(writer *http.ResponseWriter, request *http.Request) {
+	//store the site info to db
+	db := MySQLConnectionHelper()
+	//defer db.Close()
+
+	//parse the request body
+	var site Site
+	err := json.NewDecoder(request.Body).Decode(&site)
+	if err != nil {
+		http.Error(*writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+	//save the site info to db
+	result := db.Create(&site)
+	if result.Error != nil {
+		http.Error(*writer, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	//return the site info to client
+	json.NewEncoder(*writer).Encode(site)
+
+}
+
+func GetAllSiteInfo(writer *http.ResponseWriter, request *http.Request) {
+
+}
+
+func GetSiteInfoByID(writer *http.ResponseWriter, request *http.Request) {
+
+}
+
+func UpdateSiteInfoByID(writer *http.ResponseWriter, request *http.Request) {
+
+}
+
+func DeleteSiteInfoByID(writer *http.ResponseWriter, request *http.Request) {
+
 }
